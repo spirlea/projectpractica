4ad04c798e5d4b223f47157edb15ee50c9aae501
Station3 <Station3@LB215-Station11>
2016-05-13 10:41:09 +0300

    Add  index(navbar bootstrap,login form )

---
+++ src/MagBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MagBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('MagBundle:Default:index.html.twig');
+    }
+}
---
+++ src/MagBundle/MagBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MagBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MagBundle extends Bundle
+{
+}
